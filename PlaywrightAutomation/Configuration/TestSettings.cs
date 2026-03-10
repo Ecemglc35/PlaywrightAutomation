4b455e1c8b10735b98fb5216fb7e73bd575978be
Ecemglc35 <ecegulec35@gmail.com>
2026-03-10 11:37:20 -0400

    CI build was failed with browser issue. Made it testing headless.

--- PlaywrightAutomation/Configuration/TestSettings.cs
+++ PlaywrightAutomation/Configuration/TestSettings.cs
@@ -3,5 +3,5 @@ namespace PlaywrightAutomation.Configuration;
 public static class TestSettings
 {
     public static string homePageUrl => "https://demoqa.com";
-    public static bool headless => false;
+    public static bool headless => true;
 }
